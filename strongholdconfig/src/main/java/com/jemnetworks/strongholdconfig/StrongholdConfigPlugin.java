2dc91557433fdb51852e61e4b2d596f4f57386ba
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-08-21 13:59:02 -0500

    Initial commit

---
+++ strongholdconfig/src/main/java/com/jemnetworks/strongholdconfig/StrongholdConfigPlugin.java
@@ -0,0 +1,10 @@
+package com.jemnetworks.strongholdconfig;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StrongholdConfigPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
